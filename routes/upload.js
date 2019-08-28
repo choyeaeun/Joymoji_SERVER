383e3bac005d328b6b2d7d8945bc07c8856b0c2a
choyeaeun <silbia629@gmail.com>
2019-08-28 20:12:42 +0900

    res message key, imgURL

--- routes/upload.js
+++ routes/upload.js
@@ -4,11 +4,9 @@ var router = express.Router();
 const upload = require('../config/multer');
 
 router.post('/single', upload.single('img'), (req, res) => {
-    //const key = req.file.key;
-    //console.log(req);
-    //console.log(res.json({'key' : key, 'imageUrl' : req.file.location}));
-    //res.send({'key' : key, 'imageUrl' : req.file.location})
-    console.log(req.file);
+    //console.log(req.file);
+
+    res.json({key: req.file.key, imgURL : req.file.location });
 });
 
 module.exports = router;
