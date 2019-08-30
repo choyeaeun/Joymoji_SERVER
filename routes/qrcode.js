7f5821ad8d1ba2106f16087043f9ee134270231a
choyeaeun <silbia629@gmail.com>
2019-08-30 19:08:50 +0900

    rebuild upload

--- routes/qrcode.js
+++ routes/qrcode.js
@@ -6,6 +6,7 @@ const fs = require('fs');
 /* GET users listing. */
 router.post('/', (req, res, next)=>{
 
+  console.log(req.body);
   let qr_txt = req.body.qr_txt;
   var qr_png = qr.imageSync(qr_txt, {type: 'png'})
   let qr_code_file_name = new Date().getTime() + '.png';
