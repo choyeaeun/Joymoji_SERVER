e8905d919cfe2a669f75f1cf8e162e1292f17356
choyeaeun <silbia629@gmail.com>
2019-08-30 20:59:20 +0900

    attach DB & upload media to DB

--- routes/upload.js
+++ routes/upload.js
@@ -1,14 +1,37 @@
 var express = require('express');
 var router = express.Router();
-
 const upload = require('../config/multer');
+const mysql = require('mysql');
+const DBconfig = require('../config/DBconfig');
+const config = DBconfig.database;
 
+const pool = mysql.createPool(config);
 router.post('/single', upload.single('img'), (req, res) => {
-    //console.log(req.file);
-    const originalname = req.file.originalname;
+    let originalname = req.file.originalname;
+    let key = req.file.key;
+    let location = req.file.location;
+    
+    if(originalname == null){
+        res.status(400).send({
+            message : "Null value"
+        });
+    }else {
+        let uploadQuery = 'INSERT INTO media VALUES (?,?,?)';
+        
+        pool.getConnection((err, connection)=> {
+            connection.query(uploadQuery, [originalname, key, location], (err, result) => {
+                if (err){
+                    console.log(err);
+                }else {
+                    console.log(result);
+                    connection.release();
+                }
+            });
+        });
+    }
     console.log(originalname);
     res.json({key: req.file.key, imgURL : req.file.location, originalname : req.file.originalname });
-   
+
 });
 
 module.exports = router;
